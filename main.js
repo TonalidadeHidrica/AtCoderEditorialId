15c52fefd9b5b2e74e4a9e8dea2af38b122a9d2e
TonalidadeHidrica <47710717+TonalidadeHidrica@users.noreply.github.com>
2022-08-20 17:18:13 +0900

    Update userscript to show editorial id in the list

--- main.js
+++ main.js
@@ -3,11 +3,23 @@
 // @namespace https://github.com/TonalidadeHidrica/
 // @version 0.1
 // @description Show Editorial ID on AtCoder
+// @match https://atcoder.jp/contests/*/editorial
 // @match https://atcoder.jp/contests/*/editorial/*
 // ==/UserScript==
 
 (() => {
-	const [, id] = /\/contests\/[^/]+\/editorial\/([0-9]+)$/.exec(location.href);
-	const e = document.querySelector("h2");
-	e.insertBefore(document.createTextNode(`[${id}] `), e.firstChild);
+	console.log("hoge");
+	const getId = href => /\/contests\/[^/]+\/editorial\/([0-9]+)$/.exec(href)?.[1];
+	const insert = (p, c, id) => p.insertBefore(document.createTextNode(`[${id}] `), c);
+	const id = getId(location.href);
+	if (id) {
+		const e = document.querySelector("h2");
+		insert(e, e.firstChild, id);
+	} else {
+		for (const li of document.querySelectorAll("h3 + ul > li")) {
+			const a = li.getElementsByTagName("a")[0];
+			const id = getId(a.href);
+			if(id) insert(li, a, id);
+		}
+	}
 })();
